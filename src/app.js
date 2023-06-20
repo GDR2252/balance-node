c424a751e3d730188a7c9825137378e99c767b1d
Abhinav <>
2023-06-20 23:54:38 +0530

    adding new routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const path = require('path');
 const logger = require('log4js').getLogger(path.parse(__filename).name);
+const connectDB = require('./config/dbConn');
 const navigationRouter = require('./routes/navigation');
 const fetchNavDataRouter = require('./routes/getnavdata');
 const fetchThemesRouter = require('./routes/getthemes');
+const registerRouter = require('./routes/register');
+const authRouter = require('./routes/auth');
 
 const app = express();
-
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/navigation', navigationRouter);
 app.use('/getnavdata', fetchNavDataRouter);
 app.use('/getthemes', fetchThemesRouter);
+app.use('/register', registerRouter);
+app.use('/auth', authRouter);
 
 // error handler
 // eslint-disable-next-line no-unused-vars
