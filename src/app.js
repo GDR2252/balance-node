4acce551159d361be97c5cae210447e7f1d00afe
Abhinav <>
2023-06-21 19:59:55 +0530

    update error

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use('/auth', authRouter);
 // custom 404
 // eslint-disable-next-line no-unused-vars
 app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!");
+  res.status(404).send("You're Beyond The Borders!");
 });
 
 // custom error handler
