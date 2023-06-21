9bb72f1f4eb0e95a3074512c0e8cc6f81aedbad9
Abhinav <>
2023-06-21 19:58:52 +0530

    security check and error handling

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const path = require('path');
 const logger = require('log4js').getLogger(path.parse(__filename).name);
 const connectDB = require('./config/dbConn');
@@ -10,6 +11,8 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 connectDB();
+app.use(helmet());
+app.disable('x-powered-by');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,15 +22,17 @@ app.use('/getthemes', fetchThemesRouter);
 app.use('/register', registerRouter);
 app.use('/auth', authRouter);
 
-// error handler
+// custom 404
+// eslint-disable-next-line no-unused-vars
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// custom error handler
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(500);
-  logger.log(err);
-  res.json('error');
+  logger.error(err);
+  res.status(500).send('Something broke!');
 });
 
 module.exports = app;
