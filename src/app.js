4c384fcd715ad50c57547a979693bd4a1c327cd3
Abhinav <>
2023-06-26 02:13:32 +0530

    auth changes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ const path = require('path');
 const xss = require('xss-clean');
 const logger = require('log4js').getLogger(path.parse(__filename).name);
 const connectDB = require('./config/dbConn');
-const { authenticate } = require('./config/authentication');
+const { authenticate } = require('./middleware/authentication');
+const verifyJWT = require('./middleware/verifyJWT');
 const navigationRouter = require('./routes/navigation');
 const fetchNavDataRouter = require('./routes/getnavdata');
 const fetchThemesRouter = require('./routes/getthemes');
@@ -24,7 +25,7 @@ app.use(actuator());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   const result = authenticate(req);
   if (result) {
     logger.info('Access Verified!');
@@ -32,13 +33,13 @@ app.use((req, res, next) => {
   } else {
     res.status(401).json({ message: 'not authorized' });
   }
-});
-
+}); */
+app.use('/register', registerRouter);
+app.use('/auth', authRouter);
+app.use(verifyJWT);
 app.use('/navigation', navigationRouter);
 app.use('/getnavdata', fetchNavDataRouter);
 app.use('/getthemes', fetchThemesRouter);
-app.use('/register', registerRouter);
-app.use('/auth', authRouter);
 
 // custom 404
 // eslint-disable-next-line no-unused-vars
