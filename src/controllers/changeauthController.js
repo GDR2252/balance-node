4d1642adcbf132ff1007deabe74d4087d79384d7
Abhinav <>
2023-06-29 23:17:15 +0530

    updated error message

--- src/controllers/changeauthController.js
+++ src/controllers/changeauthController.js
@@ -6,7 +6,7 @@ async function changepass(req, res) {
   const { oldPwd, pwd } = req.body;
   if (!oldPwd || !pwd) return res.status(400).json({ message: 'Old Password and New Password are required.' });
   const foundUser = await Trader.findOne({ username: req.user }).exec();
-  if (!foundUser) return res.status(401).json({ message: 'The username or old password is incorrect.' });
+  if (!foundUser) return res.status(401).json({ message: 'Old password is incorrect.' });
   const match = await bcrypt.compare(oldPwd, foundUser.password);
   if (match) {
     const hashedPwd = await bcrypt.hash(pwd, 10);
@@ -14,7 +14,7 @@ async function changepass(req, res) {
     await foundUser.save();
     res.status(201).json({ success: 'New password updated successfully!' });
   } else {
-    res.status(401).json({ message: 'The username or old password is incorrect.' });
+    res.status(401).json({ message: 'Old password is incorrect.' });
   }
 }
 
