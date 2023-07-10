90c0313ec2d843a6203eb28ecda26c03fc8a87c7
Abhinav <>
2023-07-11 02:05:50 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -32,9 +32,9 @@ app.disable('x-powered-by');
 app.use(compression());
 app.use(xss());
 app.use(actuator());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/scrapdata', require('./routes/scrapdata'));
