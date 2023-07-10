b3bea4f6e5673c4386a0a1e87faa63e7166b1e3a
skSamip <ryan.lee36152@gmail.com>
2023-07-11 00:24:41 +0530

    add some changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,16 @@
-const User = require("../model/User");
+const User = require('../model/User');
 
 const getProfile = async (req, res) => {
   const profile = await User.findOne({ username: req.user }).exec();
-  if (!profile)
-    return res.status(401).json({ message: "User id is incorrect." });
+  if (!profile) return res.status(401).json({ message: 'User id is incorrect.' });
   //   delete profile._id;
-  delete profile.__v;
-  delete profile.password;
-  res.json({ profile });
+
+  const data = {
+    username: profile.username,
+    balance: profile.balance,
+    exposure: profile.exposureLimit,
+  };
+  res.json({ data });
 };
 
 module.exports = { getProfile };
