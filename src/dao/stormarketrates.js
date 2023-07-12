551619addc4600605bac766fd4b010490915bca8
Abhinav <>
2023-07-12 11:32:50 +0530

    Update stormarketrates.js

--- src/dao/stormarketrates.js
+++ src/dao/stormarketrates.js
@@ -6,17 +6,21 @@ require('dotenv').config();
 async function storemarketrates(marketrates) {
   const uri = process.env.MONGO_URI;
   const client = new MongoClient(uri);
-  try {
-    await client.connect();
-    const result = await client.db(process.env.EXCH_DB).collection(process.env.MR_COLLECTION)
-      .insertMany(marketrates);
-    logger.info(`${result.insertedCount} new listings created with the following ids:`);
-    logger.info(result.insertedIds);
-  } catch (err) {
-    logger.error(err);
-  } finally {
-    await client.close();
-  }
+  marketrates.forEach(async (element) => {
+    try {
+      await client.connect();
+      const filter = { marketId: element.marketId };
+      const update = element;
+      const result = await client.db(process.env.EXCH_DB).collection(process.env.MR_COLLECTION)
+        .findOneAndUpdate(filter, update, { upsert: true });
+      logger.info(`${result.insertedCount} new listings created with the following ids:`);
+      logger.info(result.insertedIds);
+    } catch (err) {
+      logger.error(err);
+    } finally {
+      await client.close();
+    }
+  });
 }
 
 module.exports = { storemarketrates };
