9552fd72f1e174ca3a955f94b8c8a4d3759ed526
Abhinav <>
2023-07-12 20:03:51 +0530

    Update stormarketrates.js

--- src/dao/stormarketrates.js
+++ src/dao/stormarketrates.js
@@ -10,9 +10,16 @@ async function storemarketrates(marketrates) {
     try {
       await client.connect();
       const filter = { marketId: element.marketId };
-      const update = element;
+      const update = {
+        state: element.state,
+        runners: element.runners,
+        marketId: element.marketId,
+        isMarketDataVirtual: element.isMarketDataVirtual,
+        isMarketDataDelayed: element.isMarketDataDelayed,
+        highWaterMark: element.highWaterMark,
+      };
       const result = await client.db(process.env.EXCH_DB).collection(process.env.MR_COLLECTION)
-        .findOneAndUpdate(filter, { $set: { update } }, { upsert: true });
+        .findOneAndUpdate(filter, update, { upsert: true });
       logger.info(`${result.insertedCount} new listings created with the following ids:`);
       logger.info(result.insertedIds);
     } catch (err) {
