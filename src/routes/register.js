fd2b8c1f7278125ca5b2120f4e325d572bd7e6c7
Abhinav <>
2023-07-14 15:10:56 +0530

    Update register.js

--- src/routes/register.js
+++ src/routes/register.js
@@ -8,7 +8,7 @@ router.post('/', auth, registerController.handleNewUser);
 router.post('/generateotp', auth, registerController.generateotp);
 router.post('/verifyotp', auth, registerController.verifyotp);
 router.post('/resendotp', auth, registerController.resendOtp);
-router.post('/forgotpassword', registerController.forgotpassword);
-router.post('/verifyforgotpassword', registerController.verifyforgotpassword);
+router.post('/forgotpassword', auth, registerController.forgotpassword);
+router.post('/verifyforgotpassword', auth, registerController.verifyforgotpassword);
 
 module.exports = router;
