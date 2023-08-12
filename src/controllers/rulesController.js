6a1286f73814dc1d9f4a673ffdede051383e7153
Abhinav <>
2023-08-12 20:53:13 +0530

    Update rulesController.js

--- src/controllers/rulesController.js
+++ src/controllers/rulesController.js
@@ -161,7 +161,7 @@ async function fetchrules(req, res) {
         },
       },
     ]);
-    res.status(201).json(rules);
+    res.status(200).json(rules);
   } catch (err) {
     logger.error(err);
     res.status(500).json({ message: err.message });
