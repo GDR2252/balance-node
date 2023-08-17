4d4048dd55d5b4b255f0505c5bbb50b807b09bf2
Abhinav <137391551+abhinav-raj-freelancer@users.noreply.github.com>
2023-08-17 22:39:45 +0530

    Update betController.js

--- src/controllers/betController.js
+++ src/controllers/betController.js
@@ -108,8 +108,8 @@ async function placebet(req, res) {
     logger.info('Waited for 5 secs.');
     userdata = await client.db(process.env.EXCH_DB).collection('users').findOne({ username: req.user });
     // , { session }
-    balance = userdata.balance;
-    if (balance < Number(stake)) return res.status(401).json({ message: 'Cannot place bet. Balance is insufficient.' });
+    /* balance = userdata.balance;
+    if (balance < Number(stake)) return res.status(401).json({ message: 'Cannot place bet. Balance is insufficient.' }); */
     await client.db(process.env.EXCH_DB).collection('cricketbetplaces').insertOne({
       username: req.user,
       exEventId,
@@ -129,7 +129,6 @@ async function placebet(req, res) {
     logger.info(`Placed bet for user: ${req.user}`);
     const balanceexposures = [];
     const oldbalanceexposures = [];
-    // let placebetcondition = true;
     const plData = await client.db(process.env.EXCH_DB).collection('cricketpls').find({
       exMarketId,
       username: req.user,
@@ -161,7 +160,6 @@ async function placebet(req, res) {
       }, {}));
       prevVal = Math.min(...oldbalanceexposures);
       newVal = Math.min(...balanceexposures);
-      // placebetcondition = newVal < prevVal;
       const filter = { _id: plData[0]._id };
       const update = { selectionId: result };
       await client.db(process.env.EXCH_DB).collection('cricketpls').updateOne(
@@ -176,8 +174,9 @@ async function placebet(req, res) {
       username: req.user,
     }).toArray();
     // , { session }
-    /* balance = userdata.balance;
-    if (balance < Number(stake) && !placebetcondition) return res.status(401).json({ message: 'Cannot place bet. Balance is insufficient.' }); */
+    const placebetcondition = newVal < exposure;
+    balance = userdata.balance;
+    if (balance < Number(stake) && !placebetcondition) return res.status(401).json({ message: 'Cannot place bet. Balance is insufficient.' });
     if (!exposureData.length > 0) {
       await client.db(process.env.EXCH_DB).collection('exposuremanages').insertOne({
         exEventId,
