500e0ae55dfefd5c8fa4e4d05c2ce275d647fd49
samip <kothiyasamip@gmail.com>
2023-08-21 23:43:10 +0530

    update error message

--- src/controllers/streamSheduleController.js
+++ src/controllers/streamSheduleController.js
@@ -1,21 +1,13 @@
-const { default: axios } = require('axios');
 const path = require('path');
 const logger = require('log4js').getLogger(path.parse(__filename).name);
 const StreamShedule = require('../model/StreamShedule');
 require('dotenv').config();
 
 async function getStream(req, res) {
-//   await axios.get('https://ss247.life/api/7e0f63439c55a393c8d7eebaafe826093f235f3e/streaminfo.php').then(async (response) => {
-//     console.log(response.data); // Data received from the URL
-//     await StreamShedule.insertMany(response.data.data.getMatches);
-//   })
-//     .catch((error) => {
-//       console.error('Error:', error);
-//     });
-
-  const { eventId } = req.query;
+    const { eventId } = req.query;
+    
   const profile = await StreamShedule.findOne({ MatchID: eventId }).select('Channel').exec();
-  if (!profile) return res.status(404).json({ message: 'No Event found yet!' });
+  if (!profile) return res.status(200).json({ Channel: null });
   try {
     res.status(200).json(profile);
   } catch (err) {
