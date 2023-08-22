97a112b7cf33d324d5a2bdf0dda6ba892d8e99ce
samip <kothiyasamip@gmail.com>
2023-08-22 11:30:37 +0530

    update cron job

--- src/cronjob/streamShedular.js
+++ src/cronjob/streamShedular.js
@@ -4,50 +4,32 @@ const StreamShedule = require('../model/StreamShedule');
 
 // Define the task you want to run
 const task = async () => {
-  console.log('Cron job running at:', new Date());
-  // Add your task logic here
-  //   console.log('STREAM_SCHEDULE_URL', process.env.STREAM_SCHEDULE_URL);
-
-  await axios.get('https://ss247.life/api/7e0f63439c55a393c8d7eebaafe826093f235f3e/streaminfo.php').then(async (response) => {
-    // console.log(response.data); // Data received from the URL
-
-    if (response.data.data.getMatches.length > 0) {
-      for (const item of response.data.data.getMatches) {
-        const { MatchID } = item;
-
-        // Use updateOne with upsert option
-        await StreamShedule.updateOne({ MatchID }, item, { upsert: true });
-        // console.log(`Updated or created data for matchid: ${MatchID}`);
+   try {
+    console.log('Cron job running at:', new Date());
+    let respData = [];
+    //   console.log('STREAM_SCHEDULE_URL', process.env.STREAM_SCHEDULE_URL);
+    await axios.get('https://ss247.life/api/7e0f63439c55a393c8d7eebaafe826093f235f3e/streaminfo.php').then(async (response) => {
+      if (response.data.data.getMatches.length > 0) {
+        respData = response?.data?.data?.getMatches;
       }
+    })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    if (respData.length > 0) {
+      const bulkOps = respData.map((item) => ({
+        updateOne: {
+          filter: { MatchID: item.MatchID },
+          update: item,
+          upsert: true,
+        },
+      }));
+      await StreamShedule.bulkWrite(bulkOps);
+      console.log('Bulk update/creation completed.');
     }
-
-    // const bulkOps = [];
-
-    // eslint-disable-next-line no-restricted-syntax
-    // for (const item of response.data.data.getMatches) {
-    // // Create an update operation with upsert option
-    //   const updateOperation = {
-    //     updateOne: {
-    //       filter: { MatchID: item.MatchID },
-    //       update: item,
-    //       upsert: true,
-    //     },
-    //   };
-
-    //   bulkOps.push(updateOperation);
-    // }
-
-    // if (bulkOps.length > 0) {
-    // // Use bulkWrite to perform batch operations
-    //   await StreamShedule.bulkWrite(bulkOps);
-    //   console.log('Bulk update/creation completed');
-    // }
-
-    // await StreamShedule.insertMany(response.data.data.getMatches);
-  })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  } catch (error) {
+    console.log('error', error);
+  }
 };
 
 // Schedule the cron job to run every 15 minutes
