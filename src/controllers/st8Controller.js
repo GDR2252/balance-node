212c7080d88800ddfb897d54436f8cbee0c4a1ff
Nikunj <nikunj@Nikunjs-MacBook-Pro.local>
2023-08-25 00:55:19 +0530

    added 3 apis

--- src/controllers/st8Controller.js
+++ src/controllers/st8Controller.js
@@ -12,8 +12,7 @@ var pub_pem = rsu.readFile('./ec.pub');
 var pub_key = rs.KEYUTIL.getKey(pub_pem);
 
 async function signBody(req, res) {
-    const data = req.body
-    const reqString = JSON.stringify(req.body, null, 1)
+    const reqString = JSON.stringify(req.body.data, null)
     var sign = new rs.KJUR.crypto.Signature({alg: 'SHA256withECDSA'});
     sign.init(priv_key);
     var sign_hex = sign.signString(reqString);
@@ -29,21 +28,97 @@ async function signBody(req, res) {
 
     console.log('Signature valid?:');
     console.log(is_valid);
-    console.log(reqString);
+    
     
 
     if(is_valid){
-        axios.post("https://cbt001.o.p8d.xyz/api/operator/v1/cashier/deposit",
-        reqString,
-        // "{\"transaction_id\":\"adr6702abc4b4t7ae6b971979abhd817c987\",\"player\":\"user_1234\",\"amount\":91.4,\"currency\":\"INR\",\"site\":\"cbtf\"}", 
-        {
-            "Content-Type" : "application/json;charset=utf-8",
-            "x-st8-sign" : sign_b64
-        }).then(res => console.log(res.data)).catch(err => console.log(err))
+        let data = JSON.stringify(req.body.data);
+          let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: req.body.url,
+            headers: { 
+              'Content-Type': 'application/json', 
+              'x-st8-sign': sign_b64
+            },
+            data : data
+          };
+          
+          axios.request(config)
+          .then((response) => {
+            console.log(JSON.stringify(response.data));
+            return res.send({is_valid, st8Response: response.data })
+          })
+          .catch((error) => {
+            console.log(error.response.data);
+            return res.send({is_valid, st8Error: error.response.data })
+          });
+    }else{
+        return res.send({message: "Something went wrong!" })
     }
-    return res.send({is_valid, sign_b64, reqString })
 }
 
 module.exports = {
     signBody
   };
+
+// 1. Game Lanuch API 
+
+// {
+//     "data" : {
+//        "game_code": "gmz_go_wild",
+//         "currency": "INR",
+//         "site": {
+//         "id": "cbtf",
+//         "lobby": "https://cbt001.o.p8d.xyz/games",
+//         "deposit": "https://cbt001.o.p8d.xyz/cashier"
+//         },
+//             "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5pa3VuaiIsImlhdCI6MTY5Mjg3NTAzMCwiZXhwIjoxNjkyOTYxNDMwfQ.ylP_RgJZ_Xxf_avzDOlTUXreTGZArN4sWyJwAHEH9To",
+//             "player": "user_1234",
+//             "country": "IN",
+//             "lang": "eng",
+//             "device": "DESKTOP",
+//             "fun_mode": false,
+//             "player_profile": {
+//             "id": "user_1234",
+//             "jurisdiction": "CeG",
+//             "default_currency": "INR",
+//             "reg_country": "IN",
+//             "affiliate": "aff_5678",
+//             "bet_limits": "low",
+//             "birth_date": "1999-04-01",
+//             "reg_date": "2022-04-10",
+//             "attributes": {
+//             "labels": [
+//             "table game preference",
+//             "language English"
+//             ]
+//             }
+//         }
+//     },
+//     "url" : "https://cbt001.o.p8d.xyz/api/operator/v1/launch"
+// }
+
+
+// 2. Get balance API 
+// {
+//     "data" : {
+//         "player": "user_1234",
+//         "currency": "INR",
+//         "site": "cbtf",
+//         "developer_code": "btsl"
+//     },
+//     "url" : "https://cbt001.o.p8d.xyz/api/operator/v1/cashier/balance"
+// }
+
+// 3. Deposit API 
+// {
+//     "data" : {
+//         "transaction_id": "adr4702abc4b4t7ae6b571979abhd817c987",
+//         "player": "user_1234",
+//         "amount": 11.6,
+//         "currency":"INR",
+//         "site": "cbtf"
+//     },
+//     "url" : "https://cbt001.o.p8d.xyz/api/operator/v1/cashier/deposit"
+// }
