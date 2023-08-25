3476da2a4d2c2d046a7c7b54da81097a9db397dc
Abhinav <137391551+abhinav-raj-freelancer@users.noreply.github.com>
2023-08-25 18:46:57 +0530

    post -> get

--- src/routes/bet.js
+++ src/routes/bet.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const betController = require('../controllers/betController');
 
 router.post('/', betController.placebet);
-router.post('/fetch/cricket', betController.fetchCricket);
-router.post('/fetch/cricket/pl', betController.fetchPl);
+router.get('/fetch/cricket', betController.fetchCricket);
+router.get('/fetch/cricket/pl', betController.fetchPl);
 router.get('/history', betController.history);
 module.exports = router;
--- src/routes/list.js
+++ src/routes/list.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { auth } = require('../middleware/authentication');
 const listController = require('../controllers/listController');
 
-router.post('/sportsList', auth, listController.sportsList);
-router.post('/sideMenuList', auth, listController.sideMenuList);
+router.get('/sportsList', auth, listController.sportsList);
+router.get('/sideMenuList', auth, listController.sideMenuList);
 router.get('/getEventList', auth, listController.getEventList);
 router.get('/getMarketList', auth, listController.getMarketList);
 module.exports = router;
