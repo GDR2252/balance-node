a90cbffd7f52b3e2efee5b6942d8530a5fe73a47
samip <kothiyasamip@gmail.com>
2023-09-17 18:50:37 +0530

    update stream cron

--- src/cronjob/streamShedular.js
+++ src/cronjob/streamShedular.js
@@ -1,6 +1,7 @@
 const { default: axios } = require('axios');
 const cron = require('node-cron');
 const StreamShedule = require('../model/StreamShedule');
+const Market = require('../model/Market');
 const connectDB = require('../config/dbConn');
 
 // Define the task you want to run
@@ -17,15 +18,22 @@ const task = async () => {
       .catch((error) => {
         console.error('Error:', error);
       });
+    const eventIds = [];
+    const bulkOps = [];
     if (respData.length > 0) {
-      const bulkOps = respData.map((item) => ({
-        updateOne: {
-          filter: { MatchID: item.MatchID },
-          update: item,
-          upsert: true,
-        },
-      }));
+      respData.map((item) => {
+        eventIds.push(item.MatchID);
+        bulkOps.push({
+          updateOne: {
+            filter: { MatchID: item.MatchID },
+            update: item,
+            upsert: true,
+          },
+        });
+      });
       await StreamShedule.bulkWrite(bulkOps);
+      await Market.updateMany({ eventId: { $in: eventIds } }, { isStreaming: true });
+        
       console.log('Bulk update/creation completed.');
     } else {
       console.log('No match data to update.');
@@ -36,4 +44,4 @@ const task = async () => {
 };
 
 // Schedule the cron job to run every 15 minutes
-cron.schedule('*/15 * * * *', task);
+cron.schedule('*/1 * * * *', task);
