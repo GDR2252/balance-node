006ae539eecd1bcb6e5fcafc076c419b262cf487
samip <kothiyasamip@gmail.com>
2023-09-20 17:21:13 +0530

    add jwt verify in token get api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const { auth } = require('../middleware/authentication');
 const authController = require('../controllers/authController');
 
+const verifyJWT = require('../middleware/verifyJWT');
+
 router.post('/', auth, authController.handleLogin);
-router.get('/gettoken', authController.generateToken);
+router.get('/gettoken', verifyJWT, authController.generateToken);
 
 module.exports = router;
