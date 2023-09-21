0edda33af73a7b853cec59c3540fe90782f7ee96
Abhinav <137391551+abhinav-raj-freelancer@users.noreply.github.com>
2023-09-21 14:54:11 +0530

    Update betController.js

--- src/controllers/betController.js
+++ src/controllers/betController.js
@@ -51,8 +51,8 @@ async function placebet(req, res) {
       logger.info(runners[i].state.status);
       logger.info(selectionId);
       logger.info(runners[i].selectionId);
-      logger.info(runners[i].selectionId === selectionId);
-      if (runners[i].selectionId === selectionId) { logger.info('Updating selectionstatus'); selectionStatus = runners[i].state.status; logger.info(selectionStatus); }
+      logger.info(runners[i].selectionId.toString() === selectionId);
+      if ((runners[i].selectionId).toString() === selectionId) { logger.info('Updating selectionstatus'); selectionStatus = runners[i].state.status; logger.info(selectionStatus); }
     }
     logger.info(state.inplay);
     logger.info(isPreBet);
